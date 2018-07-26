17572f5b4f97ca80a80382f344771b433143f3d1
biewenxiang <biewenxiang@gmail>
2018-07-26 16:32:42 +0800

    定时任务

--- src/main/java/com/xss/admin/ServeAdminApplication.java
+++ src/main/java/com/xss/admin/ServeAdminApplication.java
@@ -2,8 +2,10 @@ package com.xss.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ServeAdminApplication {
 
 	public static void main(String[] args) {
