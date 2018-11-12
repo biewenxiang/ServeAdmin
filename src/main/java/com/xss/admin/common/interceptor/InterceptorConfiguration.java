6ed5a8296dc9258eed9a6d4c659c75bdab0243b2
biewx <biewenxiang>
2018-11-12 11:50:40 +0800

    测试

--- src/main/java/com/xss/admin/common/interceptor/InterceptorConfiguration.java
+++ src/main/java/com/xss/admin/common/interceptor/InterceptorConfiguration.java
@@ -8,6 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class InterceptorConfiguration extends WebMvcConfigurerAdapter {
 	@Override
 	public void addInterceptors(InterceptorRegistry registry){
-		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**");
+		registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/**").excludePathPatterns("/static/**");
 	}
 }
