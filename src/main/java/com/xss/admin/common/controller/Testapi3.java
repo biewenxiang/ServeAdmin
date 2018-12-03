11c7b78d3d30e735a9065aeaa4666021dd624fce
biewx <biewenxiang>
2018-12-03 15:14:37 +0800

    bb1

--- src/main/java/com/xss/admin/common/controller/Testapi3.java
+++ src/main/java/com/xss/admin/common/controller/Testapi3.java
@@ -3,5 +3,7 @@ package com.xss.admin.common.controller;
 public class Testapi3 {
     private static String aa= " ";
     private static String bb= " ";
+    private static String bb= " ";
+    private static String bb= " ";
 
 }
