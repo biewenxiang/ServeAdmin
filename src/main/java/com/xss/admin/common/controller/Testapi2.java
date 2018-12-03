b15fafb481cce443223f7fd3cf27f18ea38623b7
biewx <biewenxiang>
2018-12-03 14:57:50 +0800

    dd0

--- src/main/java/com/xss/admin/common/controller/Testapi2.java
+++ src/main/java/com/xss/admin/common/controller/Testapi2.java
@@ -3,6 +3,6 @@ package com.xss.admin.common.controller;
 public class Testapi2 {
     private static String aa ="helloworld";
     private static String cc ="helloworld";
-    private static String dd ="helloworldd";
+    private static String dd ="helloworld";
 
 }
